6b7791b4240029eb384fd4e2626e3b036d2e2cc3
JefO <jef.ogu@gmail.com>
2023-05-16 15:04:14 -0400

    Update server.js
    added path to /notes

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
-const express = require('express')
+const express = require('express');
+const path = require('path');
+const api  = require('./routes/index');
 
 const PORT = 3001;
 
@@ -7,12 +9,17 @@ const app = express();
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
+app.use('/api', api);
 
 app.use(express.static('public'));
 
 // GET Route for homepage
 app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
 app.listen(PORT, () =>
